536d896151d264c8da33ed6939f720000ad16937
Jayalakshmi999 <72653579+Jayalakshmi999@users.noreply.github.com>
2022-12-14 20:33:41 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const connectDatabase = require('./config/db');
+const bodyParser = require('body-parser')
+const path = require('path');
+const cors=require('cors');
+
+const app = express()
+connectDatabase()
+
+app.use(express.json({ extended:false}))
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+
+const userRoute = require('./routes/user');
+const studentRoute = require('./routes/student');
+const bookRoute = require('./routes/book')
+const loanRoute = require('./routes/loan')
+
+app.use('/user',userRoute);
+app.use('/student', studentRoute);
+app.use('/book',bookRoute)
+app.use('/loan', loanRoute);
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    app.get('*', (req,res) =>{
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+    }
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT,() =>{
+    console.log(`Server running on port ${PORT}`)
+})
